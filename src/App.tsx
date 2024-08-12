5b091444ebf7a075eb585ad2d266159b81c91843
charliejp21 <charliejp21@gmail.com>
2024-08-12 02:28:09 -0600

    feat: [Primeros ajustes con tailwind]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-
+import { menuItems } from "./data/db"
+import { MenuItem } from "./components/MenuItem"
 function App() {
+
+  console.log(menuItems)
   
   return (
     <>
-      <h1>Hola mundo</h1>
+      <header className="bg-teal-400 py-5">
+
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumos</h1>
+
+      </header>
+
+      <main className="max-w-7xl max-auto py-20 grid md:grid-cols-2">
+
+        <div className="p-5">
+
+         <h2 className="text-4xl font-black">Menú</h2>
+
+          <div className="space-y-3 mt-10">
+
+            {menuItems.map(item => (
+
+                <MenuItem 
+              
+                  key={item.id} 
+                  item={item} />
+
+              ))}
+
+         </div>
+
+        </div>
+  
+        <div>
+
+          <h2>Consumo</h2>
+
+        </div>
+
+      </main>
+
     </>
   )
+
 }
 
 export default App
